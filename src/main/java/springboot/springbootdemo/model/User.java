81e32a401815e41f358ddf6977567c5571e4fc5b
Aselya <aseldrkz@mail.ru>
2024-11-10 13:37:34 +0600

    0

--- src/main/java/springboot/springbootdemo/model/User.java
+++ src/main/java/springboot/springbootdemo/model/User.java
@@ -19,7 +19,16 @@ public class User {
 
     @Column(name = "last_name")
     private String lastName;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/springboot/springbootdemo/repository/UserRepository.java
+++ src/main/java/springboot/springbootdemo/repository/UserRepository.java
@@ -3,6 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import springboot.springbootdemo.model.User;
 
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+
 
 }
